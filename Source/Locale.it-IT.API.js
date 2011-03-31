908effc87a858f5169955a921c3a9a25dd7ae67f
Arian <arian@aryweb.nl>
2011-03-31 12:20:24 +0200

    Italian

---
+++ Source/Locale.it-IT.API.js
@@ -0,0 +1,23 @@
+/*
+---
+name: Locale.en-GB.API
+description: API Translations in GB English
+requires: [Locale.API]
+provides: Locale.en-GB.API
+...
+*/
+
+(function(){
+
+Locale.define('it-IT', 'API', {
+	Window: {
+		addEvent: {
+			method: 'pizza',
+			arguments: [{
+				domready: 'pasta'
+			}]
+		}
+	}
+});
+
+})();
